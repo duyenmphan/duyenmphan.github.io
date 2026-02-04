86ca20a2c6cf96b3f77bdc91742446007d0fff7b
kimhungbui <kimhungbui@users.noreply.github.com>
2026-02-04 15:41:16 +0000

    deploy: c03bb29b880ceb16b4159c3d6630995b0d7c8b9c

---
+++ src/content_option.js
@@ -0,0 +1,129 @@
+const logotext = "Phan My Duyen";
+const meta = {
+    title: "Phan My Duyen",
+    description: "I'm Phan My Duyen a senior student at University of Social Science and Humanities - Vietnam National University Ho Chi Minh City studying Anthropology.",
+};
+
+const introdata = {
+    title: "Phan My Duyen",
+    animated: {
+        first: "",
+        second: "",
+    },
+    description: "In the past, " ,
+    // description_two:"  I am currently a first year Computer Science (MS) student at Stanford and I did my undergrad at Cornell studying Information Science. I have received grants and scholarships for my research like the Robert S Ann Morley Research Grant and the Gwanjeong Scholarship for my Master's Studies.",
+    your_img_url: "",
+};
+
+const dataabout = {
+    title: "Research Interests",
+    aboutme: "My research is primarily focused on ",
+};
+
+
+
+// const services = [{
+//         title: "SDG AI Lab - Software Engineer",
+//         period:"March 2023 - May 2023",
+//         description: "Development of the Digital Technologies Radar : a Frontier Technology Radar for Disaster Risk Reduction (FTR4DRR), which allows for the systematic tracking and understanding of frontier technologies as they are developed. https://github.com/SDG-AI-Lab/Digital_Technologies_Radar",
+//     },
+//     {
+//         title: "Inspirit AI - AI/ML Instructor",
+//         period:"March 2020 - ",
+//         description: "Conducted lectures on Neural Networks, Computer Vision, Deep Learning, Reinforcement Learning and helped highschool students work on programming ML Models",
+//     },
+//     {
+//         title: "Cornell Data Science - Insights Team",
+//         period:"Janurary 2020 - December 2022 ",
+//         description: "Insights Team - Conducted and led Data Science/Machine Learning Projects, Conducted lectures on Neural Networks for Freshman Onboarding INFO 1998 ML lectures ",
+//     },
+// ];
+
+const awards = [{
+    title: "Gwanjeong Educational Foundation Scholarship - Masters Degree",
+    period:"Sep 2023 - June 2025",
+},
+{
+    title: "ACM/IEEE Human Robot Interaction (HRI 2023) - Best Student Paper Award",
+    period:"March 2022",
+},
+{
+    title: "Robert S Ann Morley Research Grant - Cornell University",
+    period:" December 2021 ",
+},
+];
+
+const workexperience = [{
+    title: "a",
+    period:"a",
+    description: "a",
+},
+
+];
+
+const researchexperience = [{
+    title: "KIXLab (Prof. Juho Kim) - Generative Agents for Programming",
+    period:"June 2023 - ",
+    description: "Developed a LLM Teachable Agent (Algobo) for Programming Education. Developed prompting techniques for Knowledge State Configuration and conducted LLM Agent Evaluation on different knowledge states. Submitted to CHI 2024",
+},
+
+];
+
+const dataportfolio = [
+    {
+        img: "altCanvas",
+        title: "AltCanvas: A Tile-Based Image Editor with Generative AI for Blind or Visually Impaired People",
+        authors: "Phan My Duyen, Maho Kohga, Steve Landau, Sile O'Modhrain, Hari Subramonyam",
+        conference: "ACM SIGACCESS Conference on Computers and Accessibility, ASSETS 2024",
+        doi:"https://doi.org/10.48550/arXiv.2408.10240",
+        video:"https://www.youtube.com/watch?v=tJUqjjwSxPs",
+        project:"https://shljessie.github.io/AltCanvas/",
+        code:"coming soon",
+    },
+
+];
+
+const projectportfolio = [
+    {
+        title: "DarakBooks - LLM based Book Recommendation",
+        description: "Backend Development of LLM Based Book Recommendation API, User Book Club Groups API - Typescript, Nestjs, Swagger",
+        github: "https://github.com/darakbooks-project/darakbooks-BE",
+        website: "https://darakbooks.vercel.app/onboarding"
+    },
+
+
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "shl1027@stanford.edu",
+    YOUR_FONE: "-",
+    description: "- ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/shljessie",
+    facebook: "https://facebook.com",
+    scholar: "https://scholar.google.com/citations?user=POepUzkAAAAJ&hl=en",
+    linkedin: "https://www.linkedin.com/in/seonghee-lee/",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    researchexperience,
+    workexperience,
+    worktimeline,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    projectportfolio,
+    logotext,
+    awards,
+};
